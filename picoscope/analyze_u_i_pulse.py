95005774df792659d1c0bab2b1ecbc12527beb80
peerlevi <orzelek@hm.edu>
2025-10-31 11:55:42 +0100

    Add new scripts for Picoscope data acquisition and analysis; include CSV reading and processing functionalities

--- picoscope/analyze_u_i_pulse.py
+++ picoscope/analyze_u_i_pulse.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 
 # ===================== CONTROL =====================
 BASE_DIR     = r"C:\Users\mext\Documents\02 Python Schnittstelle STM32 serielle Steuerung\mext_cap_testbench_control_code\picoscope"
-RUN_NAME     = "Pulse_Test_10V_Source_10"   # muss zum Messlauf passen (CSV + meta.json)
+RUN_NAME     = "Picoscope_Oszi_Tool_Export"   # muss zum Messlauf passen (CSV + meta.json)
 USE_LAST     = True            # True: neuesten pulse_id verwenden; False: PULSE_ID nutzen
 PULSE_ID     = 3               # nur wenn USE_LAST=False
 
